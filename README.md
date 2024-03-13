9aa92594ec879fb1444c8ac1dfe2c614749c1b1c
jayfang88 <jayfang88@gmail.com>
2024-03-13 13:54:33 -0700

    Create words-of-affirmation npm package

---
+++ README.md
@@ -0,0 +1,12 @@
+## **Installation**
+
+``` bash
+npm i words-of-affirmation 
+```
+
+## **Usage**
+
+``` js
+const words = require('words-of-affirmation');
+console.log(words);
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+function wordsOfAffirmation() {
+    return "You are deeply loved, valued beyond measure, " +
+    "and your presence brightens the lives of those around you. " +
+    "Let go of stress and worry, for you are supported and cherished. " +
+    "Embrace each moment with joy and gratitude, knowing that you are enough " +
+    "just as you are. Your journey is filled with boundless opportunities, " +
+    "and you have the strength and resilience to overcome any challenges " +
+    "that may come your way. So, take a deep breath, smile, and trust that " +
+    "you are worthy of all the wonderful things life has in store for you."
+}
+
+module.exports = wordsOfAffirmation
